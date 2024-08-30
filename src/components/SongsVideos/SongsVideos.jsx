dd568671acf505e3b2b1bc281eaa01a5e035d762
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2024-08-30 12:01:29 +0200

    beta 1.1 fully working

--- src/components/SongsVideos/SongsVideos.jsx
+++ src/components/SongsVideos/SongsVideos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./style.scss";
@@ -6,80 +6,8 @@ import "./style.scss";
 const SongsVideos = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const fetchSongs = async () => {
-      try {
-        const response = await axios.get("/songs/songs.json");
-        setSongs(response.data); // Set the fetched songs data
-      } catch (error) {
-        console.error("Error fetching songs:", error);
-      }
-    };
-
-    fetchSongs();
-
-    const intervalId = setInterval(fetchSongs, 5000);
-    return () => clearInterval(intervalId);
-  }, []);
-
-  const playSong = (videoId) => {
-    if (audio) {
-      audio.pause();
-    }
-    if (isPlaying && currentVideoId === videoId) {
-      setIsPlaying(false);
-      return;
-    }
-
-    const songUrl = `server-weld-one.vercel.app/songs/${encodeURIComponent(
-      videoId
-    )}.mp3`;
-
-    const newAudio = new Audio(songUrl);
-    setAudio(newAudio);
-    setCurrentVideoId(videoId);
-
-    newAudio.play().then(() => {
-      setIsPlaying(true);
-    });
-
-    newAudio.onended = () => {
-      setIsPlaying(false);
-    };
-  };
-
-  const toggleLike = async (videoId) => {
-    const updatedSongs = songs.map((song) => {
-      const key = Object.keys(song)[0]; // Get the key (index) of the song
-      if (song[key].videoId === videoId) {
-        return {
-          [key]: {
-            ...song[key],
-            liked: !song[key].liked,
-          },
-        };
-      }
-      return song;
-    });
-    setSongs(updatedSongs);
-
-    try {
-      await axios.post("server-weld-one.vercel.app/api/like", {
-        id: videoId,
-        liked:
-          updatedSongs.find(
-            (song) => song[Object.keys(song)[0]].videoId === videoId
-          )[
-            Object.keys(
-              updatedSongs.find(
-                (song) => song[Object.keys(song)[0]].videoId === videoId
-              )
-            )
-          ].liked || false,
-      });
-    } catch (error) {
-      console.error("Error updating like status:", error);
-    }
+  const handleToggle = () => {
+    setIsVisible(!isVisible);
   };
 
   return (
